367e1bb9bbd72c3640157a68855b971011fa2473
myanhon <pluh@protonmail.com>
2020-09-01 21:13:02 +0200

    added update/delete methods with some validation

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.example.demo.dao.PersonDao;
 import com.example.demo.model.Person;
@@ -24,5 +26,17 @@ public class PersonService {
 
     public List<Person> getAllPeople(){
         return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id){
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id){
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person person){
+        return personDao.updatePersonById(id, person);
     }
 }
